74d18c1ce57f6c1b7b93ea11d8b00f7691c5a0af
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2026-05-28 20:50:09 +0700

    Update demo routes for vue-ssg-core 2.0.0

--- apps/demo/src/routes.ts
+++ apps/demo/src/routes.ts
@@ -1,11 +1,23 @@
-import { createDocsRoutes } from '@codemonster-ru/vue-ssg-core'
+import { createDocsRoutesFromManifest } from '@codemonster-ru/vue-ssg-core'
 import DocsHomeView from '@/views/DocsHomeView.vue'
 import DocsNotFoundView from '@/views/DocsNotFoundView.vue'
 import DocsPageView from '@/views/DocsPageView.vue'
 import { docsHome, docsPages } from '@/content/docs'
 
-export const routes = createDocsRoutes({
-  docsPages,
+const docsRoutes = docsPages.map((page) => ({
+  id: page.id,
+  path: page.path,
+  sourcePath: page.sourcePath,
+  isIndexPage: page.isIndexPage,
+  title: page.title,
+  navTitle: page.navTitle,
+  description: page.description,
+  order: page.order,
+  section: page.section
+}))
+
+export const routes = createDocsRoutesFromManifest({
+  docsRoutes,
   homeEnabled: docsHome.enabled,
   homeComponent: DocsHomeView,
   pageComponent: DocsPageView,
